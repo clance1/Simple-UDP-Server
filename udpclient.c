015fc373714bd830047415dc16508eac9a767b77
Carson Lance <clance1@nd.edu>
2019-09-11 13:27:41 -0400

    added files and lib

---
+++ udpclient.c
@@ -0,0 +1,18 @@
+/* udpclient.c */
+
+#include <stdio.h>
+#include <sys/socket.h>
+
+#include "pg1lib.h"
+
+#DEFINE SERVER_PORT 41026;
+
+CLIENT_USAGE[] = \
+  "Usage: student0[0/1/2/6].cse.nd.edu 41026 [filename.txt or \"message\"]";
+
+
+
+int main(int argc, char* argv[]) {
+
+  return EXIT_SUCCESS;
+}
